0931abdf312dfc6b5adc8a679413058dae46b8bb
Davy ROBERT <davyrobert90@gmail.com>
2024-05-13 10:32:18 +0200

    display countries with emoji

--- frontend/src/components/Countries.tsx
+++ frontend/src/components/Countries.tsx
@@ -10,10 +10,19 @@ const Countries = () => {
    if (error) return <p>Error : {error.message}</p>;
    console.log('countries : ', data);
 
-   
-  return (
-    <div>Countries list !!</div>
-  )
+
+   return (
+      <div className='countriesList'>
+         {data?.countries.map((country, index) => (
+            <div key={country.id}>
+               <h4>{country.name}</h4>
+               {<img src={country.emoji} alt="flag" />}
+               {/* { country.emoji ? <img src={country.emoji} alt="flag" /> : <img src='https://urlz.fr/qDVA' alt="flag" />  } */}
+               {/* <img src='https://urlz.fr/qDVA' alt="flag" /> */}
+            </div>
+         ))}
+      </div>
+   )
 }
 
 export default Countries
