1b1528b555c4951dc7afb81489849851a6394bbb
Davy ROBERT <davyrobert90@gmail.com>
2024-05-13 09:43:21 +0200

    add all GraphQL queries

--- frontend/src/graphql/client.ts
+++ frontend/src/graphql/client.ts
@@ -1,1 +1,69 @@
-// TODO
+import { gql } from "@apollo/client";
+
+export const GET_COUNTRIES = gql`
+query Countries {
+  countries {
+    id
+    code
+    name
+    emoji
+    continent {
+      name
+      id
+    }
+  }
+}
+`
+
+
+export const GET_ONE_COUNTRY = gql`
+query Country($code: String!) {
+  country(code: $code) {
+    id
+    code
+    name
+    emoji
+    continent {
+      id
+      name
+    }
+  }
+}
+`
+
+
+export const GET_CONTINENTS = gql`
+query Continents {
+  continents {
+    id
+    name
+  }
+} 
+`
+
+
+export const CREATE_NEW_COUNTRY = gql`
+mutation Mutation($data: NewCountryInput!) {
+  addCountry(data: $data) {
+    id
+    code
+    name
+    emoji
+    continent {
+      id
+      name
+    }
+  }
+}
+`
+
+
+
+export const CREATE_NEW_CONTINENT = gql`
+mutation AddContinent($data: NewContinentInput!) {
+  addContinent(data: $data) {
+    id
+    name
+  }
+}
+`
